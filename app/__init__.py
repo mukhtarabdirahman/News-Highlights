858da528cc31b8258ce0362534fda9e6966f5409
mukhtarabdirahman <mukhtarabdirahmanm@gmail.com>
2019-12-27 20:11:04 +0300

    creating blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,9 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
     
-    
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
